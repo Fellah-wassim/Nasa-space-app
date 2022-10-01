35c2a5072da5117b8941b2af977f76bfbed1fa1b
Yasser-Belatreche <yasser.belatreche0@gmail.com>
2022-10-01 14:46:35 +0100

    fix general file structure

--- src/js/drowing-board.js
+++ src/js/drowing-board.js
@@ -1,26 +1,28 @@
 const canvas = document.querySelector("canvas"),
-toolBtns = document.querySelectorAll(".tool"),
-fillColor = document.querySelector("#fill-color"),
-sizeSlider = document.querySelector("#size-slider"),
-colorBtns = document.querySelectorAll(".colors .option"),
-colorPicker = document.querySelector("#color-picker"),
-clearCanvas = document.querySelector(".clear-canvas"),
-saveImg = document.querySelector(".save-img"),
-ctx = canvas.getContext("2d");
+    toolBtns = document.querySelectorAll(".tool"),
+    fillColor = document.querySelector("#fill-color"),
+    sizeSlider = document.querySelector("#size-slider"),
+    colorBtns = document.querySelectorAll(".colors .option"),
+    colorPicker = document.querySelector("#color-picker"),
+    clearCanvas = document.querySelector(".clear-canvas"),
+    saveImg = document.querySelector(".save-img"),
+    ctx = canvas.getContext("2d");
 
 // global variables with default value
-let prevMouseX, prevMouseY, snapshot,
-isDrawing = false,
-selectedTool = "brush",
-brushWidth = 5,
-selectedColor = "#000";
+let prevMouseX,
+    prevMouseY,
+    snapshot,
+    isDrawing = false,
+    selectedTool = "brush",
+    brushWidth = 5,
+    selectedColor = "#000";
 
 const setCanvasBackground = () => {
     // setting whole canvas background to white, so the downloaded img background will be white
     ctx.fillStyle = "#fff";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = selectedColor; // setting fillstyle back to the selectedColor, it'll be the brush color
-}
+};
 
 window.addEventListener("load", () => {
     // setting canvas width/height.. offsetwidth/height returns viewable width/height of an element
@@ -31,20 +33,22 @@ window.addEventListener("load", () => {
 
 const drawRect = (e) => {
     // if fillColor isn't checked draw a rect with border else draw rect with background
-    if(!fillColor.checked) {
+    if (!fillColor.checked) {
         // creating circle according to the mouse pointer
         return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
     }
     ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
-}
+};
 
 const drawCircle = (e) => {
     ctx.beginPath(); // creating new path to draw circle
     // getting radius for circle according to the mouse pointer
-    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
+    let radius = Math.sqrt(
+        Math.pow(prevMouseX - e.offsetX, 2) + Math.pow(prevMouseY - e.offsetY, 2)
+    );
     ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI); // creating circle according to the mouse pointer
     fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill circle else draw border circle
-}
+};
 
 const drawTriangle = (e) => {
     ctx.beginPath(); // creating new path to draw circle
@@ -53,7 +57,7 @@ const drawTriangle = (e) => {
     ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY); // creating bottom line of triangle
     ctx.closePath(); // closing path of a triangle so the third line draw automatically
     fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill triangle else draw border
-}
+};
 
 const startDraw = (e) => {
     isDrawing = true;
@@ -65,29 +69,30 @@ const startDraw = (e) => {
     ctx.fillStyle = selectedColor; // passing selectedColor as fill style
     // copying canvas data & passing as snapshot value.. this avoids dragging the image
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
-}
+};
 
 const drawing = (e) => {
-    if(!isDrawing) return; // if isDrawing is false return from here
+    if (!isDrawing) return; // if isDrawing is false return from here
     ctx.putImageData(snapshot, 0, 0); // adding copied canvas data on to this canvas
 
-    if(selectedTool === "brush" || selectedTool === "eraser") {
-        // if selected tool is eraser then set strokeStyle to white 
+    if (selectedTool === "brush" || selectedTool === "eraser") {
+        // if selected tool is eraser then set strokeStyle to white
         // to paint white color on to the existing canvas content else set the stroke color to selected color
         ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
         ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse pointer
         ctx.stroke(); // drawing/filling line with color
-    } else if(selectedTool === "rectangle"){
+    } else if (selectedTool === "rectangle") {
         drawRect(e);
-    } else if(selectedTool === "circle"){
+    } else if (selectedTool === "circle") {
         drawCircle(e);
     } else {
         drawTriangle(e);
     }
-}
+};
 
-toolBtns.forEach(btn => {
-    btn.addEventListener("click", () => { // adding click event to all tool option
+toolBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        // adding click event to all tool option
         // removing active class from the previous option and adding on current clicked option
         document.querySelector(".options .active").classList.remove("active");
         btn.classList.add("active");
@@ -95,10 +100,11 @@ toolBtns.forEach(btn => {
     });
 });
 
-sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value); // passing slider value as brushSize
+sizeSlider.addEventListener("change", () => (brushWidth = sizeSlider.value)); // passing slider value as brushSize
 
-colorBtns.forEach(btn => {
-    btn.addEventListener("click", () => { // adding click event to all color button
+colorBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        // adding click event to all color button
         // removing selected class from the previous option and adding on current clicked option
         document.querySelector(".options .selected").classList.remove("selected");
         btn.classList.add("selected");
@@ -127,4 +133,4 @@ saveImg.addEventListener("click", () => {
 
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mousemove", drawing);
-canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
